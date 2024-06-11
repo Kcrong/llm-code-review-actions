a3b14c5242c515050e5a3974529f4b1e8fe15de9
Kcrong <henry@magicx.ai>
2024-06-12 04:04:20 +0900

    Fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/Kcrong/groq-code-review-actions/pkg/prompt"
 	"github.com/magicx-ai/groq-go/groq"
@@ -9,6 +10,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 const (
@@ -17,15 +22,21 @@ const (
 )
 
 func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatalln("GITHUB_TOKEN environment variable is not set")
+		return
+	}
+
 	apiKey := os.Getenv("GROQ_API_KEY")
 	if apiKey == "" {
-		log.Fatalf("GROQ_API_KEY environment variable not set")
+		log.Fatalln("GROQ_API_KEY environment variable not set")
 	}
 
 	// Read the PR diff
 	diff, err := os.ReadFile(filepath.Join(os.Getenv("GITHUB_WORKSPACE"), defaultDiffName))
 	if err != nil {
-		log.Fatalf("Error reading diff: %+v", err)
+		log.Fatalf("Error reading diff: %+v\n", err)
 		return
 	}
 
@@ -38,7 +49,44 @@ func main() {
 		log.Fatalf("Error running action: %+v", err)
 	}
 
-	fmt.Println(results)
+	if err := createComment(token, results); err != nil {
+		log.Fatalf("Error creating comment: %+v", err)
+	}
+}
+
+func createComment(token string, content string) error {
+	owner := os.Getenv("GITHUB_REPOSITORY_OWNER")
+	repo := os.Getenv("GITHUB_REPOSITORY_NAME")
+	prNumber := os.Getenv("GITHUB_PR_NUMBER")
+
+	if owner == "" || repo == "" || prNumber == "" {
+		return errors.New("one or more required environment variables are not set")
+	}
+
+	// Create a new GitHub client
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// Create a new comment
+	comment := &github.IssueComment{
+		Body: &content,
+	}
+
+	prNum, err := strconv.Atoi(prNumber)
+	if err != nil {
+		return errors.Wrap(err, "error converting PR number")
+	}
+
+	_, _, err = client.Issues.CreateComment(ctx, owner, repo, prNum, comment)
+	if err != nil {
+		return errors.Wrap(err, "error creating comment")
+	}
+
+	return nil
 }
 
 type RunParameters struct {
